e099f143a532baa13a1844d729ea692ccc4fde40
Kailash Chander Pathak <you@example.com>
2024-06-28 09:39:43 +0530

    Added tc for login

--- cypress/e2e/Page/loginPage.cy.js
+++ cypress/e2e/Page/loginPage.cy.js
@@ -1,4 +1,77 @@
 
+class LoginPage {
+constructor(){
+this.url ='https://www.saucedemo.com/'
+this.userName ='#user-name'
+this.password ='#password'
+this.loginButton='#login-button'
+}
+openUrl()
+{
+cy.visit(this.url)
+}
+enterUsername(){
+//cy.get(this.userName).clear()
+//cy.get(this.userName).type(standard_user);
+cy.get(this.userName).clear().type(standard_user);
+}
+enterPassword() {
+    //cy.get(this.password).clear()
+    //cy.get(this.password).type(secret_sauce);
+    cy.get(this.password).clear().type(secret_sauce)
+}
+
+clickLoginButton(){
+    cy.get(this.loginButton).click()
+}
+
+loginValidation(){
+    cy.get(this.userName).clear().type(standard_user11);
+    cy.get(this.password).clear().type(secret_sauce22)
+    cy.get(this.loginButton).click()
+    cy.get('data-test="error-button"').should('have.text','Username and password do not match')
+    //cy.contains('Epic sadface: Username and password do not match any user in this service')
+
+}
 
 
-// Login Into the application
+
+
+
+}
+module.exports = LoginPage
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Login {
+//   constructor() {
+//     this.url =
+//       "https://www.saucedemo.com/";
+//   }
+
+//   enterUrl() {
+//     cy.visit(this.url);
+//   }
+//   enterEmaill(userName) {
+//     cy.get('#user-name').type(userName)
+//   }
+
+//   enterPassword(password) {
+//     cy.get('#password').type(password)
+//   }
+
+//   clickLoginButton() {
+//     cy.get('#login-button').click()
+//   }
+// }
+// module.exports = Login;
---
+++ cypress/e2e/Tests/addRemoveProduct.cy.js
@@ -0,0 +1,39 @@
+/// <reference types="cypress" />
+import login from "../Page/loginPage.cy";
+const loginpage = new login();
+
+describe("Test Suite : Login Into the Portal with valid crediential", () => {
+  it("Login Into the Site", () => {
+    loginpage.openUrl();
+    loginpage.enterUsername();
+    loginpage.enterPassword();
+    loginpage.loginButton();
+  });
+  it("Login Into the Site with Invalid crediential", () => {
+    loginpage.openUrl();
+    loginpage.loginValidation()
+  });
+});
+
+// import Login from "../Page/loginPage.cy";
+// const loginPage = new Login();
+
+// describe("Test Cas for login and Logout", () => {
+//   let testdata
+//   beforeEach(function () {
+//     loginPage.enterUrl();
+//     cy.fixture('crediential').then( (testdata1)=> {
+//       testdata = testdata1;
+//     });
+//   });
+//   it("Login Into the Application", () => {
+//     loginPage.enterEmaill(testdata.email);
+//     loginPage.enterPassword(testdata.password);
+//     loginPage.clickLoginButton();
+//     cy.contains("Products");
+//   });
+//   it("Logout ", () => {
+//     cy.get("#react-burger-menu-btn").click();
+//     cy.get("#logout_sidebar_link").click();
+//   });
+// });
